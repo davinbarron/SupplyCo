a97d9504c8351b40417cedbaa1ec8fa5ba76f54f
davinbarron <davinbarron667@gmail.com>
2025-10-04 16:28:52 +0100

    Further refactoring. Introducing MainApp.kt. Updating AndroidManifest.xml

--- app/src/main/java/org/wit/supplyco/activities/SupplierActivity.kt
+++ app/src/main/java/org/wit/supplyco/activities/SupplierActivity.kt
@@ -2,15 +2,22 @@ package org.wit.supplyco.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.wit.supplyco.R
-import timber.log.Timber
+import org.wit.supplyco.databinding.ActivitySupplierBinding
+import org.wit.supplyco.main.MainApp
+import timber.log.Timber.i
 
 class SupplierActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySupplierBinding
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        Timber.plant(Timber.DebugTree())
-        Timber.i("Main Activity started..")
+        binding = ActivitySupplierBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        app = application as MainApp
+        i("Supplier Activity started..")
     }
 }
