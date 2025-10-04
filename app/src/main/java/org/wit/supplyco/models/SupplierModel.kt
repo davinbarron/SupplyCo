279e8f258ed419f43e50e7a2a08f322c8df14f79
davinbarron <davinbarron667@gmail.com>
2025-10-04 16:34:57 +0100

    New supplier model created.

---
+++ app/src/main/java/org/wit/supplyco/models/SupplierModel.kt
@@ -0,0 +1,9 @@
+package org.wit.supplyco.models
+
+data class SupplierModel (
+    var name: String = "",
+    var description: String = "",
+    var contact: String = "",
+    var email: String = "",
+    var address: String = ""
+)
