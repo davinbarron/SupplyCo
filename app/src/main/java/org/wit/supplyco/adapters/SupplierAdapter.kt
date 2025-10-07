2f1ca2d7b9a6330378c97167b52b28b06293f0b7
davinbarron <davinbarron667@gmail.com>
2025-10-07 17:19:52 +0100

    SupplierAdapter.kt binds the data from suppliers to the supplier card created in card_supplier.xml, the cards references are held which will be recycled in our view.

--- app/src/main/java/org/wit/supplyco/adapters/SupplierAdapter.kt
+++ app/src/main/java/org/wit/supplyco/adapters/SupplierAdapter.kt
@@ -1,4 +1,37 @@
 package org.wit.supplyco.adapters
 
-class SupplierAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.wit.supplyco.databinding.CardSupplierBinding
+import org.wit.supplyco.models.SupplierModel
+
+class SupplierAdapter constructor(private var suppliers: List<SupplierModel>) :
+    RecyclerView.Adapter<SupplierAdapter.SupplierViewHolder>() {
+
+    class SupplierViewHolder(private val binding : CardSupplierBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(supplier: SupplierModel) {
+            binding.supplierName.text = supplier.name
+            binding.supplierDescription.text = supplier.description
+            binding.supplierContact.text = supplier.contact
+            binding.supplierEmail.text = supplier.email
+            binding.supplierAddress.text = supplier.address
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SupplierViewHolder {
+        val binding = CardSupplierBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return SupplierViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SupplierViewHolder, position: Int) {
+        val supplier = suppliers[holder.adapterPosition]
+        holder.bind(supplier)
+    }
+
+    override fun getItemCount(): Int = suppliers.size
 }
