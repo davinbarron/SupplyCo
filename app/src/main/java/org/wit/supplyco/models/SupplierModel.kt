ab93637369de2e32621de6cb8ce506f01dd8fc0a
davinbarron <davinbarron667@gmail.com>
2025-10-20 16:27:34 +0100

    Creating a supplier repository to handle supplier model functions.

--- app/src/main/java/org/wit/supplyco/models/SupplierModel.kt
+++ app/src/main/java/org/wit/supplyco/models/SupplierModel.kt
@@ -5,9 +5,10 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class SupplierModel (
+    var supplierId: Long = 0,
     var name: String = "",
     var description: String = "",
     var contact: String = "",
     var email: String = "",
     var address: String = ""
-)
+) : Parcelable
