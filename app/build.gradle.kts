f338344e815c8deae5a4c730e55c2f67e0607909
davinbarron <davinbarron667@gmail.com>
2025-10-22 13:37:11 +0100

    Setting up Gson dependencies in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.gson)
 
     implementation(libs.timber)
 
