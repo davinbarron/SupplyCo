50b0dc128e95d62b432af039133c805015c05376
davinbarron <davinbarron667@gmail.com>
2025-12-11 23:39:56 +0000

    Item model

---
+++ app/src/main/java/org/wit/supplyco/models/ItemModel.kt
@@ -0,0 +1,13 @@
+package org.wit.supplyco.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ItemModel(
+    var id: String? = null,
+    var name: String = "",
+    var amount: Int = 0,
+    var price: Double = 0.0,
+    var releaseDate: Long? = null
+) : Parcelable
